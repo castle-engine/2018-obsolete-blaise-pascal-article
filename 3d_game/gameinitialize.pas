ade50a527659173ec86581e3dbf4ce752011f7e2
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-08-20 17:42:36 +0200

    Load level, walk camera

--- 3d_game/gameinitialize.pas
+++ 3d_game/gameinitialize.pas
@@ -4,11 +4,12 @@ interface
 
 implementation
 
-uses CastleWindow, CastleScene, CastleFilesUtils, CastleKeysMouse;
+uses CastleWindow, CastleScene, CastleFilesUtils, CastleKeysMouse,
+  CastleVectors, CastleCameras, CastleStringUtils;
 
 var
   Window: TCastleWindow;
-  SoldierScene: TCastleScene;
+  LevelScene, SoldierScene: TCastleScene;
 
 procedure WindowPress(Container: TUIContainer; const Event: TInputPressRelease);
 begin
@@ -19,7 +20,11 @@ begin
     SoldierScene.PlayAnimation('stand', true)
   else
   if Event.IsKey('3') then
-    SoldierScene.PlayAnimation('die', true);
+    SoldierScene.PlayAnimation('die', true)
+  else
+  if Event.IsKey(CtrlM) then
+    Window.SceneManager.WalkCamera.MouseLook :=
+      not Window.SceneManager.WalkCamera.MouseLook;
 end;
 
 procedure ApplicationInitialize;
@@ -30,9 +35,24 @@ begin
   SoldierScene.Load(ApplicationData('character/soldier1.castle-anim-frames'));
   SoldierScene.ProcessEvents := true;
   SoldierScene.PlayAnimation('walk', true);
-
   Window.SceneManager.Items.Add(SoldierScene);
-  Window.SceneManager.MainScene := SoldierScene;
+
+  LevelScene := TCastleScene.Create(Application);
+  LevelScene.Load(ApplicationData('level/level-dungeon.x3d'));
+  LevelScene.Spatial := [ssRendering, ssDynamicCollisions];
+  LevelScene.Attributes.PhongShading := true;
+  Window.SceneManager.Items.Add(LevelScene);
+
+  Window.SceneManager.MainScene := LevelScene;
+
+  Window.SceneManager.NavigationType := ntWalk;
+  Window.SceneManager.WalkCamera.MoveSpeed := 10;
+  Window.SceneManager.WalkCamera.SetView(
+    Vector3(9.69, 1.72, 8.12), // position
+    Vector3(0.87, 0.00, -0.49), // direction
+    Vector3(0.00, 1.00, 0.00), // up (current)
+    Vector3(0.00, 1.00, 0.00) // gravity up
+  );
 end;
 
 initialization
