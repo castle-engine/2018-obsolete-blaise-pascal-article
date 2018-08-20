054ac233d7e2506b21c1af2c552d4761d0671648
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-08-20 20:20:20 +0200

    TEnemy class

--- 3d_game/gameinitialize.pas
+++ 3d_game/gameinitialize.pas
@@ -4,38 +4,70 @@ interface
 
 implementation
 
-uses CastleWindow, CastleScene, CastleFilesUtils, CastleKeysMouse,
-  CastleVectors, CastleCameras, CastleStringUtils;
+uses Classes,
+  CastleWindow, CastleScene, CastleFilesUtils, CastleKeysMouse,
+  CastleVectors, CastleCameras, CastleStringUtils, CastleTransform;
 
 var
   Window: TCastleWindow;
-  LevelScene, SoldierScene: TCastleScene;
+  LevelScene: TCastleScene;
+
+type
+  TEnemy = class(TCastleTransform)
+  public
+    SoldierScene: TCastleScene;
+    MoveDirection: Integer; //< Always 1 or -1
+    constructor Create(AOwner: TComponent); override;
+    procedure Update(const SecondsPassed: Single; var RemoveMe: TRemoveType); override;
+  end;
+
+constructor TEnemy.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  MoveDirection := -1;
+
+  SoldierScene := TCastleScene.Create(Application);
+  SoldierScene.Load(ApplicationData('character/soldier1.castle-anim-frames'));
+  SoldierScene.ProcessEvents := true;
+  SoldierScene.PlayAnimation('walk', true);
+
+  Add(SoldierScene);
+end;
+
+procedure TEnemy.Update(const SecondsPassed: Single; var RemoveMe: TRemoveType);
+const
+  MovingSpeed = 2;
+begin
+  inherited;
+
+  // We modify the Z coordinate, responsible for enemy going forward
+  Translation := Translation +
+    Vector3(0, 0, MoveDirection * SecondsPassed * MovingSpeed);
+
+  // Toggle MoveDirection between 1 and -1
+  if Translation.Z > 5 then
+    MoveDirection := -1
+  else
+  if Translation.Z < -5 then
+    MoveDirection := 1;
+end;
 
 procedure WindowPress(Container: TUIContainer; const Event: TInputPressRelease);
 begin
-  if Event.IsKey('1') then
-    SoldierScene.PlayAnimation('walk', true)
-  else
-  if Event.IsKey('2') then
-    SoldierScene.PlayAnimation('stand', true)
-  else
-  if Event.IsKey('3') then
-    SoldierScene.PlayAnimation('die', true)
-  else
   if Event.IsKey(CtrlM) then
     Window.SceneManager.WalkCamera.MouseLook :=
       not Window.SceneManager.WalkCamera.MouseLook;
 end;
 
 procedure ApplicationInitialize;
+var
+  Enemy: TEnemy;
 begin
   Window.OnPress := @WindowPress;
 
-  SoldierScene := TCastleScene.Create(Application);
-  SoldierScene.Load(ApplicationData('character/soldier1.castle-anim-frames'));
-  SoldierScene.ProcessEvents := true;
-  SoldierScene.PlayAnimation('walk', true);
-  Window.SceneManager.Items.Add(SoldierScene);
+  Enemy := TEnemy.Create(Application);
+  Window.SceneManager.Items.Add(Enemy);
 
   LevelScene := TCastleScene.Create(Application);
   LevelScene.Load(ApplicationData('level/level-dungeon.x3d'));
